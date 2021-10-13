1396a19d004d8aa32d3741c1e593b51020576aec
junghosub <72376781+junghosub@users.noreply.github.com>
2021-10-13 20:23:02 +0900

    Add files via upload

---
+++ 1. 백준/Silver Ⅰ/절댓값 힙.py
@@ -0,0 +1,12 @@
+import sys
+import heapq
+
+n = int(sys.stdin.readline())
+heap = []
+
+for _ in range(n):
+    x = int(sys.stdin.readline())
+    if x != 0:
+        heapq.heappush(heap, (abs(x), x))
+    else:
+        print(heapq.heappop(heap)[1] if heap else 0)
