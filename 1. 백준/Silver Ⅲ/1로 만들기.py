266f4329d866cca7f0627806b7dce27d3d999fce
정호섭 <72376781+junghosub@users.noreply.github.com>
2021-11-10 17:17:04 +0900

    Add files via upload

---
+++ 1. 백준/Silver Ⅲ/1로 만들기.py
@@ -0,0 +1,12 @@
+import sys
+
+def solution(n):
+    dp = [0 for _ in range(n+1)]
+    for i in range(2, n+1):
+        dp[i] = dp[i-1]+1
+        if i % 3 == 0:
+            dp[i] = min(dp[i], dp[i//3]+1)
+        if i % 2 == 0:
+            dp[i] = min(dp[i], dp[i//2]+1)
+    return dp[n]
+print(solution(int(sys.stdin.readline())))
